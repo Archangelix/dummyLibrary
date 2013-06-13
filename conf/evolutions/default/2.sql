22cbff5a7801f332595f711c539776f9737afa79
Iwan <iwan@ubuntu.ubuntu-domain>
2013-06-14 00:37:48 +0800

    Changing application.conf file

--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -2,19 +2,10 @@
 
 # --- !Ups
 
-CREATE SEQUENCE book_id_seq;
-
-CREATE TABLE BOOK (
-	id integer not null default nextval('book_id_seq'),
-	title varchar2(100),
-	author varchar2(50),
-	publishedYear integer
-);
-
 INSERT INTO BOOK (TITLE, AUTHOR, PUBLISHEDYEAR) VALUES ('ONE', 'SONE', 1901);
 INSERT INTO BOOK (TITLE, AUTHOR, PUBLISHEDYEAR) VALUES ('TWO', 'STWO', 1902);
 INSERT INTO BOOK (TITLE, AUTHOR, PUBLISHEDYEAR) VALUES ('THREE', 'STHREE', 1903);
 
 # --- !Downs
-DROP TABLE BOOK;
-DROP SEQUENCE book_id_seq;
+
+DELETE FROM BOOK;
