1fcd53399d4bb9ca0be5cdda2417d8bd72803966
Iwan <iwan@ubuntu.ubuntu-domain>
2013-07-29 17:14:55 +0800

    Adding the sidebar and the header in separate files.

---
+++ app/controllers/ABUserList.scala
@@ -0,0 +1,90 @@
+package controllers
+
+import models.OBCatalog
+import models.form.FormCatalog
+import play.api.Play.current
+import play.api.cache.Cache
+import play.api.data.Form
+import play.api.data.Forms.number
+import play.api.data.Forms.tuple
+import play.api.mvc.Action
+import play.api.mvc.Controller
+import play.api.mvc.Security
+import services.DBService
+
+object ABUserList extends Controller with TSecured {
+  
+  val CURRENT_PAGE = "currentPage"
+  val CURRENT_PAGE_IDX = "currentPageIdx"
+  val MAX_PAGE_IDX = "maxPageIdx"
+  val CURRENT_LIST = "currentList"
+  val ITEMS_PER_VIEW = 5 
+    
+  val listingForm = Form (
+    tuple (
+      "currentPageIdx" -> number,
+      "maxPageIdx" -> number
+    )
+  )
+  
+  /**
+   * Displaying the list of catalogs for a corresponding page index.
+   */
+  def listUsers = TODO
+  
+  /**
+   * Displaying the first page of the catalog list.
+   */
+  def navigateFirst = withAuth {username => implicit req =>
+    val (list1, rowCnt) = DBService.partialCatalogs(1)
+    Cache.set(CURRENT_PAGE, (list1, rowCnt, 1))
+    Redirect(routes.ABCatalogList.index)
+  }
+  
+  /**
+   * Displaying the previous page of the catalog list.
+   */
+  def navigatePrev = withAuth {username => implicit req =>
+    val currentPageIdx = req.queryString.get("currentPageIdx").flatMap(_.headOption).get.toInt
+    val nextPageIdx = if (currentPageIdx<=1) 1 else currentPageIdx-1
+    val (list1, rowCnt) = DBService.partialCatalogs(nextPageIdx)
+    Cache.set(CURRENT_PAGE, (list1, rowCnt, nextPageIdx))
+    Redirect(routes.ABCatalogList.index)
+  }
+  
+  /**
+   * Displaying the next page of the catalog list.
+   */
+  def navigateNext = withAuth {username => implicit req =>
+    val currentPageIdx = req.queryString.get("currentPageIdx").flatMap(_.headOption).get.toInt
+    val maxPage = req.queryString.get(MAX_PAGE_IDX).flatMap(_.headOption).get.toInt
+    val nextPageIdx = if (currentPageIdx>=maxPage) maxPage else currentPageIdx+1
+    println("maxPage = "+maxPage)
+    println("nextPage = "+nextPageIdx)
+    val (list1, rowCnt) = DBService.partialCatalogs(nextPageIdx)
+    Cache.set(CURRENT_PAGE, (list1, rowCnt, nextPageIdx))
+    Redirect(routes.ABCatalogList.index)
+  }
+  
+  /**
+   * Displaying the last page of the catalog list.
+   */
+  def navigateLast = withAuth {username => implicit req =>
+    val maxPage = req.queryString.get(MAX_PAGE_IDX).flatMap(_.headOption).get.toInt
+    val (list1, rowCnt) = DBService.partialCatalogs(maxPage)
+    Cache.set(CURRENT_PAGE, (list1, rowCnt, maxPage))
+    Redirect(routes.ABCatalogList.index)
+  }
+  
+  def edit(pIDStr: String) = TODO
+  
+  /**
+   * Removing a particular catalog from the listing page.
+   */
+  def remove(pIDStr: String) = withAuth {username => implicit req =>
+    DBService.deleteCatalog(pIDStr.toInt)
+    Redirect(routes.ABCatalogList.index())
+  }
+  
+  def isBlank(str: String) = str==null || str.trim().equals("")
+}
